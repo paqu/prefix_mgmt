94a21b95c02db59d3aae2e13b3f12df5ef2060b3
Pawel Kusz <pawel.kusz1993@gmail.com>
2025-10-27 19:58:00 +0100

    tests[add]: validate invalid mask

--- tests/test_add.cpp
+++ tests/test_add.cpp
@@ -1,3 +1,4 @@
+#include "prefix_mgmt/prefix_mgmt.h"
 #include <gtest/gtest.h>
 
 class AddFunctionTest : public ::testing::Test {
@@ -7,4 +8,22 @@ class AddFunctionTest : public ::testing::Test {
     void TearDown() override {}
 };
 
-TEST_F(AddFunctionTest, Template) { EXPECT_EQ(1, 1); }
+#if 0 
+// TC-ADD-1: Valid Prefix Addition
+TEST_F(AddFunctionTest, ValidPrefixAddition) {
+    // 10.0.0.0/8
+    // tbd later
+}
+#endif
+
+// TC-ADD-2: Invalid Mask - Below Range
+TEST_F(AddFunctionTest, InvalidMaskBelowRange) {
+    unsigned int base = 0x0A000000; // 10.0.0.0
+    EXPECT_EQ(-1, add(base, -1));
+}
+
+// TC-ADD-3: Invalid Mask - Above Range
+TEST_F(AddFunctionTest, InvalidMaskAboveRange) {
+    unsigned int base = 0x0A000000; // 10.0.0.0
+    EXPECT_EQ(-1, add(base, 33));
+}
