731124deee934fe276e1532cc1d741ee77195f18
Pawel Kusz <pawel.kusz1993@gmail.com>
2025-10-27 22:50:09 +0100

    Add validation for address alignment

--- src/prefix_mgmt.c
+++ src/prefix_mgmt.c
@@ -12,8 +12,27 @@
  */
 static bool is_valid_mask(char mask) { return mask >= 0 && mask <= 32; }
 
+/**
+ * @brief Check base alignment
+ */
+
+static bool is_aligned(unsigned int base, char mask) {
+    if (mask == 0) {
+        return base == 0;
+    }
+    if (mask == 32) {
+        return true;
+    }
+    unsigned int host_mask = (1U << (32 - mask)) - 1;
+    return (base & host_mask) == 0;
+}
+
 int add(unsigned int base, char mask) {
     if (!is_valid_mask(mask)) {
+        return -1;
+    }
+
+    if (!is_aligned(base, mask)) {
         return -1;
     }
 
