8cebc1c17b7618e293fd4984ebb8de33074e2ce7
Pawel Kusz <pawel.kusz1993@gmail.com>
2025-10-30 11:15:03 +0100

    test[del]: add basic validation tests for del() function
    - Introduced tests to check invalid mask handling

--- tests/test_del.cpp
+++ tests/test_del.cpp
@@ -1,10 +1,21 @@
+#include "prefix_mgmt/prefix_mgmt.h"
 #include <gtest/gtest.h>
 
 class DelFunctionTest : public ::testing::Test {
   protected:
-    void SetUp() override {}
+    void SetUp() override { prefix_mgmt_init(); }
 
-    void TearDown() override {}
+    void TearDown() override { prefix_mgmt_cleanup(); }
 };
 
-TEST_F(DelFunctionTest, Template) { EXPECT_EQ(1, 1); }
+// TC-DEL-1: Invalid mask below range
+TEST_F(DelFunctionTest, InvalidMaskBelowRange) {
+    unsigned int base = 0xC0AB0000; // 192.168.0.0
+    EXPECT_EQ(-1, del(base, -1));
+}
+
+// TC-DEL-2: Invalid mask above range
+TEST_F(DelFunctionTest, InvalidMaskAboveRange) {
+    unsigned int base = 0xC0AB0000; // 192.168.0.0
+    EXPECT_EQ(-1, del(base, 33));
+}
